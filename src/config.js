e2cda8c2cdac2414637235ef3a32d4d6c010b13f
Rivaldsg <lotosdesign00@gmail.com>
2025-11-29 17:15:56 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // Edit these links to point to your social profiles and contact
 export const SOCIAL_LINKS = [
-  { name: 'Instagram', url: 'https://instagram.com/your_username' },
+  { name: 'TikTok', url: 'https://www.tiktok.com/@rivaldsgn?_r=1&_t=ZM-91o4yBD9KJc' },
   { name: 'Behance', url: 'https://behance.net/your_username' },
-  { name: 'Telegram', url: 'https://t.me/Rivaldsg' },
-  { name: 'Email', url: 'mailto:you@example.com' }
+  { name: 'Telegram', url: 'https://t.me/Rivaldsgn' },
+  { name: 'YouTube', url: 'https://www.youtube.com/@RivalDesign' }
 ]
 
 // Your Telegram username for the "Make order" button (used as t.me link)
