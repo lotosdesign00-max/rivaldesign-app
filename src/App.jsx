2f22d1525326b3196e79d8994b2c04277ccad4af
Rivaldsg <lotosdesign00@gmail.com>
2025-11-30 13:59:37 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
-import Home from "./components/Home";
+import About from "./components/About";
 import Gallery from "./components/Gallery";
-import Order from "./components/Order";
 import Testimonials from "./components/Testimonials";
 import AIGenerator from "./components/AIGenerator";
+import Order from "./components/Order";
 
-function App() {
+export default function App() {
   return (
     <Router>
-      <div className="app">
-        {/* Навигация */}
-        <nav className="navbar">
-          <ul className="nav-links">
-            <li><NavLink to="/">Главная</NavLink></li>
-            <li><NavLink to="/gallery">Галерея</NavLink></li>
-            <li><NavLink to="/order">Заказать</NavLink></li>
-            <li><NavLink to="/ai">AI Генератор</NavLink></li>
-            <li><NavLink to="/testimonials">Отзывы</NavLink></li>
-          </ul>
-        </nav>
+      <nav className="nav">
+        <NavLink to="/">О нас</NavLink>
+        <NavLink to="/gallery">Галерея</NavLink>
+        <NavLink to="/testimonials">Отзывы</NavLink>
+        <NavLink to="/ai-generator">AI Генератор</NavLink>
+        <NavLink to="/order">Заказать</NavLink>
+      </nav>
 
-        {/* Основное содержимое */}
+      <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<About />} />
           <Route path="/gallery" element={<Gallery />} />
-          <Route path="/order" element={<Order />} />
-          <Route path="/ai" element={<AIGenerator />} />
           <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/ai-generator" element={<AIGenerator />} />
+          <Route path="/order" element={<Order />} />
         </Routes>
       </div>
     </Router>
   );
 }
-
-export default App;
