17c1f8325ffb8a856389e9f280064e428f6856bf
Rivaldsg <lotosdesign00@gmail.com>
2025-11-30 14:05:48 +0200

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,11 +2,10 @@ import React from "react";
 
 export default function ProjectCard({ project }) {
   return (
-    <div className="project-card">
-      <img className="project-img" src={project.image} alt={project.title} />
-      <h4>{project.title}</h4>
-      <p className="muted">{project.description}</p>
-      <p className="muted">Срок: {project.time}</p>
+    <div className="card">
+      <img src={project.image} alt={project.title} className="project-img"/>
+      <h3>{project.title}</h3>
+      <p>{project.description}</p>
     </div>
   );
 }
